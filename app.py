49f9b329bfbabcc7c87c365c0599df01b18d3906
namchuminh <chuminhnamma@gmail.com>
2023-06-18 04:07:43 +0700

    confix random file name

--- app.py
+++ app.py
@@ -31,6 +31,15 @@ model = load_model(MODEL_PATH, compile=False)
 
 class_names = open(LABELS_PATH, "r").readlines()
 
+import random
+import string
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD'], filename)
@@ -40,7 +49,8 @@ def recognize():
     file = request.files.get('image')
     if file:
         filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD'], filename)
+        file_name_random = get_random_string(12) + filename
+        filepath = os.path.join(app.config['UPLOAD'], file_name_random)
         file.save(filepath)
         
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -69,8 +79,8 @@ def recognize():
             'message': 'success',
             'class': str(class_name[2:]),
             'food_name': str(FOOD_NAME[int(class_name[2:]) - 1]),
-            'confidence': str(float(confidence_score) * 100) + "%",
-            'image_path': "http://" + BASE_URL + "/uploads" + "/" + filename,
+            'confidence': str(int(float(confidence_score) * 100)) + "%",
+            'image_path': "/uploads" + "/" + file_name_random,
             'description': summary
         })
     else:
