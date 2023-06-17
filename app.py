5ab145ced62a2aaa39dd9d1734a6e299833b3337
namchuminh <chuminhnamma@gmail.com>
2023-06-17 17:08:16 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from flask import Flask, request, jsonify, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+import numpy as np
+from keras.models import load_model 
+from PIL import Image, ImageOps  
+import wikipedia
+
+app = Flask(__name__)
+
+BASE_URL = "127.0.0.1:5000"
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
+LABELS_PATH = os.path.join(APP_ROOT, 'models/labels.txt')
+MODEL_PATH = os.path.join(APP_ROOT, 'models/best_model.h5')
+
+FOOD_NAME = [
+    'Bánh bèo', 'Bánh bột lọc', 'Bánh căn', 'Bánh chưng', 
+    'Bánh cuốn', 'Bánh đúc', 'Bánh giò', 'Bánh khọt', 'Bánh mì', 'Bánh pía', 'Bánh tét', 'Bánh tráng nướng', 
+    'Bánh xèo', 'Bún bò huế', 'Bún đậu mắm tôm', 'Bún mắm', 'Bún riêu', 'Bún thịt nướng', 'Cá kho tộ', 'Canh chua', 
+    'Cao lầu', 'Cháo lòng', 'Cơm tấm', 'Gỏi cuốn', 'Hủ tiếu', 'Mì quảng', 'Nem chua', 'Phở', 'Xôi xéo'
+]
+
+app.config['UPLOAD'] = UPLOAD_FOLDER    
+
+np.set_printoptions(suppress=True)
+
+model = load_model(MODEL_PATH, compile=False)
+
+class_names = open(LABELS_PATH, "r").readlines()
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD'], filename)
+
+@app.route('/', methods=['POST'])
+def recognize():
+    file = request.files.get('image')
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD'], filename)
+        file.save(filepath)
+        
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        
+        image = Image.open(filepath).convert("RGB")
+
+        size = (224, 224)
+        
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+        image_array = np.asarray(image)
+
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+        data[0] = normalized_image_array
+
+        prediction = model.predict(data)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+        
+        food_name = str(FOOD_NAME[int(class_name[2:]) - 1])
+        summary = wikipedia.summary(food_name)
+
+        return jsonify({
+            'message': 'success',
+            'class': str(class_name[2:]),
+            'food_name': str(FOOD_NAME[int(class_name[2:]) - 1]),
+            'confidence': str(float(confidence_score) * 100) + "%",
+            'image_path': "http://" + BASE_URL + "/uploads" + "/" + filename,
+            'description': summary
+        })
+    else:
+        return jsonify({'message': 'No file uploaded!'})
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
